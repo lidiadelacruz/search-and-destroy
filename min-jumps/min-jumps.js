70704f1c14c2dd475fd18152efbeeb0f88ebbecb
Lidia De La Cruz <lidcruz@hotmail.com>
2020-10-28 17:55:04 -0400

    Completed workshop

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -2,6 +2,19 @@
 
 // Complete this algo
 const minJumps = arr => {
+    console.log('ARRAY',arr)
+ if(arr.length<2){
+     return 0
+ } else {
+     let resultArray = []
+
+ for(let i =arr[0]; i>0;i--){
+     console.log('In the for loop', i)
+  resultArray.push(1+ minJumps(arr.slice(i)))
+ }
+ console.log('after result array', resultArray)
+return Math.min(...resultArray)
+ }
 
 };
 
